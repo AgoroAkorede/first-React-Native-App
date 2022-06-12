c0ed8e5fbb3bef4ed37c8626a6662b7509da7e4c
AgoroAkorede <agoroakorede@gmail.com>
2022-06-12 02:44:48 +0100

    Created a Guesing Game

--- App.js
+++ App.js
@@ -1,53 +1,30 @@
 import React, { useState } from "react";
-import { StyleSheet, Button, View, FlatList } from "react-native";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { StyleSheet, View} from "react-native";
+import  Header  from "./components/Header";
+import StartGameScreen from "./screens/StartGame.screen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
-
-  const addGoalHandler = (goalTitle) => {
-    setCourseGoals((currentGoals) => [
-      ...currentGoals,
-      { id: Math.random().toString(), value: goalTitle },
-    ]);
-    setIsAddMode(false);
+  const [ userNumber, setUserNumber ] = useState();
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber)
   };
 
-  const removeGoalHandler = (goalId) => {
-    setCourseGoals((currentGoals) => {
-      return currentGoals.filter((goal) => goal.id !== goalId);
-    });
-  };
-  const cancelGoalAdditionHandler = () => {
-    setIsAddMode(false);
-  };
+  let content = <StartGameScreen onStartGame={startGameHandler} />
+  if (userNumber) {
+    content=<GameScreen />
+  }
+  
   return (
     <View style={styles.screen}>
-      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
-      <GoalInput
-        visible={isAddMode}
-        onAddGoal={addGoalHandler}
-        onCancel={cancelGoalAdditionHandler}
-      />
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={(itemData) => (
-          <GoalItem
-            id={itemData.item.id}
-            onDelete={removeGoalHandler}
-            title={itemData.item.value}
-          />
-        )}
-      ></FlatList>
+      <Header title="Guess a Number" />
+     {content}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
-  },
+    flex:1
+  }
 });
